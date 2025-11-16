f2ac5c14aac2f4586432148e67826375b177dc32
Zach <WHITAKERZ@GMAIL.COM>
2025-11-16 16:24:12 -0600

    c

--- Chapter 12/Lab B/main.cpp
+++ Chapter 12/Lab B/main.cpp
@@ -1,46 +1,72 @@
 #include <iostream>
-#include <string>
-
+#include <cstring>
+#include <cctype>
 using namespace std;
 
-string upper(string s)
+// a function to change all letters in a char array to upper case
+void upper(char str[])
 {
-    for(int i = 0; i < s.length(); i++)
+    for (int i = 0; str[i] != '\0'; i++)
     {
-        s[i] = toupper(s[i]);
+        str[i] = static_cast<char>(toupper(static_cast<char>(str[i])));
     }
-    return s;
 }
 
-string lower(string s)
+// a function to change all letters in a char array to lower case
+void lower(char str[])
 {
-    for(int i = 0; i < s.length(); i++)
+    for (int i = 0; str[i] != '\0'; i++)
     {
-        s[i] = tolower(s[i]);
+        str[i] = static_cast<char>(tolower(static_cast<char>(str[i])));
     }
-    return s;
 }
 
-string flip(string s)
+// a function to flip the case of a char array
+void flip(char str[])
 {
-    for (int i = 0; i < s.length(); i++)
-        if (isupper(s.at(i)) == true) {
-            s[i] = tolower(s[i]);
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        char c = static_cast<char>(str[i]);
+        if (isupper(c))
+        {
+            str[i] = static_cast<char>(tolower(c));
         }
-        else if (islower(s.at(i)) == true) {
-            s[i] = toupper(s[i]);
+        else if (islower(c))
+        {
+            str[i] = static_cast<char>(toupper(c));
         }
-        else{cout << "error";}
-
-    return s;
+        else continue; // skip non letters
+    }
 }
 
 int main()
 {
-    string input;
-    cout << "please type a sentence or word" << endl;
-    getline(cin, input);
-    cout << upper(input) << endl;
-    cout << lower(input) << endl;
-    cout << flip(input) << endl;
+    const int SIZE = 200;
+    char input[SIZE];
+    char lowerStr[SIZE];
+    char upperStr[SIZE];
+    char flipStr[SIZE];
+
+    // get input string
+    cout << "Please type a sentence or word and I will provided it back you you in three forms." << endl;
+    cout << "All uppercase, all lowercase and I will filp the case on your input. \nEnter a sentence or word: ";
+    cin.getline(input, SIZE);
+
+    // copy original
+    strcpy(lowerStr, input);
+    strcpy(upperStr, input);
+    strcpy(flipStr, input);
+
+    // change strings
+    upper(upperStr);
+    lower(lowerStr);
+    flip(flipStr);
+
+    // output
+    cout << "Original:  " << input    << endl;
+    cout << "Uppercase: " << upperStr << endl;
+    cout << "Lowercase: " << lowerStr << endl;
+    cout << "Flipped:   " << flipStr  << endl;
+
+    return 0;
 }
