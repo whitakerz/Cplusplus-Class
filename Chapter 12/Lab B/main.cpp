1a7da8aa3684143576c79c0710a9122ac45b753a
Zach <WHITAKERZ@GMAIL.COM>
2025-11-16 11:12:26 -0600

    Update main.cpp

--- Chapter 12/Lab B/main.cpp
+++ Chapter 12/Lab B/main.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
+#include <string>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+using namespace std;
+
+string upper(string s)
+{
+    for(int i = 0; i < s.length(); i++)
+    {
+        s[i] = toupper(s[i]);
+    }
+    return s;
+}
+
+string lower(string s)
+{
+    for(int i = 0; i < s.length(); i++)
+    {
+        s[i] = tolower(s[i]);
+    }
+    return s;
+}
+
+string flip(string s)
+{
+    for (int i = 0; i < s.length(); i++)
+        if (isupper(s.at(i)) == true) {
+            s[i] = tolower(s[i]);
+        }
+        else if (islower(s.at(i)) == true) {
+            s[i] = toupper(s[i]);
+        }
+        else{cout << "error";}
+
+    return s;
+}
+
 int main()
 {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
-
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+    string input;
+    cout << "please type a sentence or word" << endl;
+    getline(cin, input);
+    cout << upper(input) << endl;
+    cout << lower(input) << endl;
+    cout << flip(input) << endl;
 }
