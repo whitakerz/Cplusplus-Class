661947c7a55c205402e5f6b40cc36d8b46c784a3
Zach <WHITAKERZ@GMAIL.COM>
2025-11-16 18:43:36 -0600

    f

--- Chapter 12/Lab B/main.cpp
+++ Chapter 12/Lab B/main.cpp
@@ -1,9 +1,16 @@
-#include <iostream>
-#include <cstring>
-#include <cctype>
+#include <iostream> // for cout and cin
+#include <cstring> // to pass character arrays
+#include <cctype> // to check if a character is upper or lower case
+#include <limits> // for numeric_limits clearing cin
 using namespace std;
 
 // a function to change all letters in a char array to upper case
+// take a character array
+// returns nothing (modifies in situ)
+
+// Truc,
+// i struggled with the difference in "(char str[])" and "(char* str)" in this function definition//
+// Zach
 void upper(char str[])
 {
     for (int i = 0; str[i] != '\0'; i++)
@@ -13,6 +20,8 @@ void upper(char str[])
 }
 
 // a function to change all letters in a char array to lower case
+// take a character array
+// returns nothing (modifies in situ)
 void lower(char str[])
 {
     for (int i = 0; str[i] != '\0'; i++)
@@ -21,7 +30,9 @@ void lower(char str[])
     }
 }
 
-// a function to flip the case of a char array
+// a function to flip the case for each character of a char array
+// take a character array
+// returns nothing (modifies in situ)
 void flip(char str[])
 {
     for (int i = 0; str[i] != '\0'; i++)
@@ -46,27 +57,47 @@ int main()
     char lowerStr[SIZE];
     char upperStr[SIZE];
     char flipStr[SIZE];
+    int choice; // variable to re-run the program or not
 
-    // get input string
-    cout << "Please type a sentence or word and I will provided it back you you in three forms." << endl;
-    cout << "All uppercase, all lowercase and I will filp the case on your input. \nEnter a sentence or word: ";
-    cin.getline(input, SIZE);
+    while (true) {
+        // get input string
+        cout << "Please type a sentence or word and I will provide it back to you in three forms." << endl;
+        cout << "All uppercase, all lowercase, and with flipped case.\n";
 
-    // copy original
-    strcpy(lowerStr, input);
-    strcpy(upperStr, input);
-    strcpy(flipStr, input);
+        cout << "Enter a sentence or word: ";
+        cin.getline(input, SIZE);
 
-    // change strings
-    upper(upperStr);
-    lower(lowerStr);
-    flip(flipStr);
+        // copy original
+        strcpy(lowerStr, input);
+        strcpy(upperStr, input);
+        strcpy(flipStr, input);
 
-    // output
-    cout << "Original:  " << input    << endl;
-    cout << "Uppercase: " << upperStr << endl;
-    cout << "Lowercase: " << lowerStr << endl;
-    cout << "Flipped:   " << flipStr  << endl;
+        // change arrays
+        upper(upperStr);
+        lower(lowerStr);
+        flip(flipStr);
 
+        // output
+        cout << "Original:  " << input    << endl;
+        cout << "Uppercase: " << upperStr << endl;
+        cout << "Lowercase: " << lowerStr << endl;
+        cout << "Flipped:   " << flipStr  << endl;
+
+        // ask to repeat
+        cout << "Would you like to try again? 1 for yes, 2 for no: ";
+
+        if (!(cin >> choice)) {
+            cout << "Invalid input. Exiting.\n";
+            break;
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        if (choice == 2) {
+            break;
+        }
+    }
+    cout << "Thank you for playing along.";
     return 0;
 }
+
